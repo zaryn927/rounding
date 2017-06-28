08a23ae572538211e8356132a92f229d1dc0f22e
Sky Link <s.z_link@yahoo.com>
2017-06-28 10:14:28 -0600

    Rounder class with int value/increment overload implemented.

---
+++ src/edu/cnm/deepdive/rounder/Rounder.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package edu.cnm.deepdive.rounder;
+
+/**
+ * @author Sky Link
+ *
+ */
+public class Rounder {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    System.out.println(round(14, 4));
+    System.out.println(round(-14, 4));
+    System.out.println(round(100, -9));
+
+  }
+  
+  public static long round(double number) {
+    return Math.round(number);
+  }
+  
+  public static int round(float number) {
+    return Math.round(number);
+  }
+  
+  public static double round(double value, double increment) {
+    return 1.00;
+  }
+  
+  public static int round(int value, int increment) {
+    int sign = Math.round(Math.signum(value));
+    value = Math.abs(value);
+    increment = Math.abs(increment);
+    int truncated = value / increment;
+    int remainder = value % increment;
+    int rounded = truncated * increment;
+    int adjustment = (2 * remainder >= increment) ? increment : 0;
+    return sign * (rounded + adjustment);
+  }
+
+}
